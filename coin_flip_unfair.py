4fac7a0933eae8467abe252a40415749c6835665
Alan O'Donohoe <alan@odonohoe.org.uk>
2016-04-06 14:36:00 +0100

    Create coin_flip_unfair.py

---
+++ coin_flip_unfair.py
@@ -0,0 +1,21 @@
+# Heads & Tails Unfair 2 12Feb2016
+import random
+
+# Ask user to choose heads or tails
+print('Player is Heads, Computer is Tails')
+pause = input("press a key to flip the coin")
+
+
+# Establish outcome of the coin toss
+coin = ['Heads', 'Tails', 'Tails', 'Tails']
+flip = random.choice(coin)
+
+# Compare choice to outcome then feedback to user
+
+if flip == 'Heads':
+    print("You win")
+
+else:
+    print("You lose")
+
+print("the coin landed on",flip)
